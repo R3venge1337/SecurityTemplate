4b6443c237041765562e6f91cdfa4274f5cb5b81
Adam Zimny <adamczyk97@op.pl>
2024-12-20 18:07:39 +0100

    Change message in error message for ACCOUNT_NOT_FOUND

--- src/main/java/com/template/az/SecurityTemplate/common/exception/error/ErrorMessages.java
+++ src/main/java/com/template/az/SecurityTemplate/common/exception/error/ErrorMessages.java
@@ -5,7 +5,7 @@ public class ErrorMessages {
     public static final String USER_NOT_FOUND = "Error: User has not been found: %s";
     public static final String ROLE_NOT_FOUND = "Error: Role has not been found!: %s";
     public static final String PERMISSION_NOT_FOUND = "Error: Permission has not been found!: %s";
-    public static final String ACCOUNT_NOT_FOUND = "Error: Permission has not been found!: %s";
+    public static final String ACCOUNT_NOT_FOUND = "Error: Account has not been found!: %s";
     public static final String ACCOUNT_IS_TAKEN = "Error: Account is already taken!: %s";
     public static final String EMAIL_IS_TAKEN = "Error: Email is already taken!: %s";
     public static final String VERIFICATION_CODE_INVALID = "Error: Verification code expired or sth went wrong! %s";
